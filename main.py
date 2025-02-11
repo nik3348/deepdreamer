6b784b9996c96696a1ecfae898c90bd0bac2e576
Nicholas Lai Jin Yung <nicklai88@live.com>
2025-02-11 19:59:03 +0800

    Added world head

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def reward_function(obs):
 
 isHuman = False
 epochs = 100
+batch_size = 16
 seq_len = 16
 d_model = 32
 exp_rate = 0.01
@@ -33,10 +34,10 @@ env = gym.make(
     "MountainCar-v0",
     render_mode="human" if isHuman else "rgb_array",
     goal_velocity=0.1,
-    max_episode_steps=256
+    max_episode_steps=batch_size * seq_len
 )
 action_dim = env.action_space.n
-model = Model(action_dim, d_model).to(device)
+model = Model(action_dim, d_model, batch_size, seq_len).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
 try:
@@ -46,11 +47,12 @@ except:
     pass
 
 for i in range(epochs):
+    obss = []
+    z_stack = []
     actions = []
     values = []
     rewards = []
     dones = []
-    losses = []
 
     tokens = torch.stack([torch.tensor(0)]).unsqueeze(0).to(device)
     obs, _ = env.reset()
@@ -59,7 +61,8 @@ for i in range(epochs):
 
     while not done or steps < 1000:
         obs = torch.tensor(obs).to(device)
-        logits, value, tokens, world_model_loss = model(obs, tokens)
+        tokens, logits, value, z, world_loss = model(
+            obs, tokens[:, -(seq_len + 1):])
         action = model.sample(logits[:, -1, :])
 
         if torch.rand(1).item() < exp_rate:
@@ -67,35 +70,38 @@ for i in range(epochs):
 
         tokens = torch.cat((tokens, action), dim=1)
 
-        if tokens.shape[1] > seq_len:
-            tokens = tokens[:, -seq_len + 1:]
-
-        obs, reward, terminated, truncated, info = env.step(action.item())
+        obs_next, reward, terminated, truncated, info = env.step(action.item())
         done = terminated or truncated
         steps += 1
 
+        obss.append(obs)
+        z_stack.append(z)
         actions.append(logits[:, -1, :])
         values.append(value[:, -1, :].item())
         rewards.append(reward + reward_function(obs))
         dones.append(done)
-        losses.append(world_model_loss)
+
+        obs = obs_next
 
         if done:
             break
 
+    obss = torch.stack(obss).to(device)
+    z_stack = torch.stack(z_stack).to(device)
     actions = torch.stack(actions).to(device)
     values = torch.tensor(values).to(device)
     rewards = torch.tensor(rewards).to(device)
     dones = torch.tensor(dones).to(device)
-    losses = torch.tensor(losses).to(device)
 
-    actor_loss, critic_loss, world_model_loss = model.calc_loss(
-        actions, rewards, dones, values, losses)
-    writer.add_scalar("World Model Loss", world_model_loss, i)
+    ae_loss = model.ae_loss(z_stack, obss)
+    actor_loss, critic_loss = model.calc_loss(
+        actions, rewards, dones, values
+    )
+
     writer.add_scalar("Actor Loss", actor_loss, i)
     writer.add_scalar("Critic Loss", critic_loss, i)
-    writer.add_scalar("Total Loss", actor_loss +
-                      critic_loss + world_model_loss, i)
+    # writer.add_scalar("World Model Loss", world_model_loss, i)
+    # writer.add_scalar("Total Loss", actor_loss + critic_loss + world_model_loss, i)
 
     if (i + 1) % 10 == 0:
         print(f"Epoch {i + 1} completed")
--- model.py
+++ model.py
@@ -32,6 +32,20 @@ class Decoder(nn.Module):
         return x
 
 
+class World(nn.Module):
+    def __init__(self, d_model):
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_model)
+        self.gelu = nn.GELU(approximate="tanh")
+        self.linear2 = nn.Linear(d_model, d_model)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.gelu(x)
+        x = self.linear2(x)
+        return x
+
+
 class Actor(nn.Module):
     def __init__(self, d_model, vocab_size):
         super().__init__()
@@ -61,12 +75,16 @@ class Critic(nn.Module):
 
 
 class Model(nn.Module):
-    def __init__(self, vocab_size, d_model):
+    def __init__(self, vocab_size, d_model, batch_size, seq_len):
         super().__init__()
         self.vocab_size = vocab_size
+        self.batch_size = batch_size
+        self.seq_len = seq_len
+
         self.encoder = Encoder(d_model)
         self.decoder = Decoder(d_model)
         self.rssm = Transformer(d_model)
+        self.world = World(d_model)
         self.actor = Actor(d_model, vocab_size)
         self.critic = Critic(d_model)
 
@@ -77,17 +95,16 @@ class Model(nn.Module):
         probs = torch.softmax(z, dim=-1)
         z_sample = torch.argmax(probs, dim=-1)
         h_next = torch.cat((h, z_sample.unsqueeze(-1).unsqueeze(-1)), dim=1)
-        world_model_loss = None
+        world_loss = None
 
         if is_training:
-            obs_pred = self.decoder(z)
-            z_pred = self.rssm(h)
-            world_model_loss = F.mse_loss(obs_pred, obs)
-            world_model_loss += F.cross_entropy(
-                z_pred.view(-1, z_pred.shape[-1]), h_next.view(-1)[1:])
+            x = self.rssm(h)
+            h_logits = self.world(x)
+            world_loss = F.cross_entropy(
+                h_logits.view(-1, h_logits.shape[-1]), h_next.view(-1)[1:])
 
-        x = self.rssm(h_next)
-        return self.actor(x), self.critic(x), h_next, world_model_loss
+        x_next = self.rssm(h_next)
+        return h_next, self.actor(x_next), self.critic(x_next), z, world_loss
 
     def sample(self, logits):
         probs = torch.softmax(logits, dim=-1)
@@ -98,15 +115,20 @@ class Model(nn.Module):
 
         return xcol
 
-    def calc_loss(self, actions, rewards, dones, values, loss):
+    def ae_loss(self, z, obss):
+        obs_preds = self.decoder(z)
+        return F.mse_loss(obs_preds, obss)
+
+    def calc_loss(self, actions, rewards, dones, values):
         gamma_ = 0.99
         lambda_ = 0.95
         eta = 0.01
 
-        actions = actions.reshape(16, 16, self.vocab_size)
-        rewards = rewards.reshape(16, 16)
-        dones = dones.reshape(16, 16)
-        values = values.reshape(16, 16)
+        actions = actions.reshape(
+            self.batch_size, self.seq_len, self.vocab_size)
+        rewards = rewards.reshape(self.batch_size, self.seq_len)
+        dones = dones.reshape(self.batch_size, self.seq_len)
+        values = values.reshape(self.batch_size, self.seq_len)
 
         lambda_returns = torch.zeros_like(rewards)
         lambda_returns[-1] = values[-1]
@@ -137,13 +159,12 @@ class Model(nn.Module):
         entropy = -(policy_probs * policy_log_probs).sum(dim=-1)
         entropy_regularization = -eta * torch.sum(entropy, dim=0).mean()
 
-        world_model_loss = loss.mean()
         actor_loss = policy_gradient_loss + entropy_regularization
         critic_loss = F.mse_loss(values, lambda_returns)
-        total_loss = actor_loss + critic_loss + world_model_loss
+        total_loss = actor_loss + critic_loss
 
         self.optimizer.zero_grad()
         total_loss.backward()
         self.optimizer.step()
 
-        return actor_loss, critic_loss, world_model_loss
+        return actor_loss, critic_loss
